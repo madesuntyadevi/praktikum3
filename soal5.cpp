a860d71f99841ac9224280931bd83bcff9f7b065
madesuntyadevi <32846022+madesuntyadevi@users.noreply.github.com>
2017-11-07 20:31:25 -0800

    Menghasilkan hasil normal

--- soal5.cpp
+++ soal5.cpp
@@ -13,5 +13,9 @@ int main () {
 	
 	if (berat<tinggi/2.5) {
   	cout << "Hasil : Kurus " << endl;
+  	
+ }
+	else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
+	cout <<  "Hasil : Normal" << endl; 
  }
 }
