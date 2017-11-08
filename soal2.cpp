b63a33fb4106c2c0126a7408437ad3ff1b7122d7
madesuntyadevi <32846022+madesuntyadevi@users.noreply.github.com>
2017-11-07 20:02:16 -0800

    Menghitung luas persegi panjang

--- soal2.cpp
+++ soal2.cpp
@@ -9,4 +9,14 @@ int main () {
 	
 	cout << "Pilih Bidang : ";
 	cin >> bidang;
+	
+	if (bidang == 'P' || bidang == 'p') {	
+		cout << "   Mengitung luas persegi panjang \n";	
+		cout << "   Masukkan panjangnya : ";
+		cin >> p;
+		cout << "   Masukkan lebarnya   : ";
+		cin >> l;
+		L = p*l;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}
 }
